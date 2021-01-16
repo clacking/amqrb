e6a9d31871d8929f324f868005f9ca236734dd9c
clacking <click@clacking.net>
2021-01-16 20:52:53 +0900

    type

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,14 +2,21 @@
 /// <reference types="next/types/global" />
 
 import Electron from 'electron';
+import { AMQEventsCommand } from './helper/AMQEvents';
+import { UserStatus } from './store/userStatusSlice';
 
 declare global {
     interface Window {
         electron: {
             invoke (channel: string, ...args: any[]): Promise<any>;
             send (channel: string, ...args: any[]): void;
+            on <E extends keyof AMQGameEvents>(channel: E, ...args: Parameters<AMQGameEvents[E]>);
             on (channel: string, listener: (event: IpcRendererEvent, ...args: any[]) => void);
             removeListener (channel: string, listener: (...listener: any[])=>void);
         }
     }
+}
+
+interface AMQGameEvents {
+    'login complete': (e: IpcRendererEvent, data: UserStatus) => void;
 }
