7f12e43f5e6d727a6f48f809c2c9f3b901752e9b
clacking <click@clacking.net>
2021-02-02 05:48:33 +0900

    delete undefined var

--- helper/AMQSocket.ts
+++ helper/AMQSocket.ts
@@ -34,8 +34,6 @@ export const initilizeGameSocket = (port: string|number, token: string): SocketI
         reconnection: true,
         upgrade: true,
         transports: ['websocket'],
-        // @ts-ignore
-        agent: proxy,
         transportOptions: { polling: { extraHeaders: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
         }}},
